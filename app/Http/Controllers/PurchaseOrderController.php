38c49aa20c1038279d96c455a4891b53fce8460f
lenberdilaw <es.itdepartment.main@gmail.com>
2025-04-11 09:15:42 +0800

    Update PurchaseOrderController.php

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -200,7 +200,7 @@ class PurchaseOrderController extends Controller
             ->orderByRaw('CAST(SUBSTRING(po_number, 4) AS UNSIGNED) DESC')
             ->first();
     
-        $latestNumber = 9900;
+        $latestNumber = 9919;
     
         if ($lastPo && preg_match('/^PO-(\d{4})$/', $lastPo->po_number, $matches)) {
             $latestNumber = (int) $matches[1];
