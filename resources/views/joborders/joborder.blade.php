3e82dcbad1c2e45ac375171d12bc26037c39babb
ES Group Software Engineer <es.itdepartment.main@gmail.com>
2025-09-30 14:42:12 +0800

    Update job date column to use job_datestart
    Changed the job date column in the job orders view to display and sort by job_datestart instead of job_date_filled for improved accuracy.

--- resources/views/joborders/joborder.blade.php
+++ resources/views/joborders/joborder.blade.php
@@ -80,8 +80,8 @@
                                     <td hidden>{{ $item->id }}</td>
                                     <td class="fw-semibold text-primary">{{ $item->job_name }}</td>
                                     <td>{{ $item->job_type }}</td>
-                                    <td data-order="{{ $item->job_date_filled }}">
-                                        {{ date('j M Y (h:i A)', strtotime($item->job_date_filled)) }}
+                                    <td data-order="{{ $item->job_datestart }}">
+                                        {{ date('j M Y (h:i A)', strtotime($item->job_datestart)) }}
                                     </td>
                                     <td>
                                         <span class="badge rounded-pill px-3 py-2 fs-6 
